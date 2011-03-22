640f3ff036cb9ab22a09b50a7ad9903a45a95a2a
marianocali <marianocali@bf05d9f8-44df-1f4b-b586-bf93beeaf494>
2011-03-22 01:00:28 +0000

    con ClienteBean

---
+++ trunk/miHomeBanking/src/com/mybank/domain/Controller.java
@@ -0,0 +1,13 @@
+package com.mybank.domain;
+
+import javax.servlet.http.HttpServlet;
+
+
+public class Controller extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
